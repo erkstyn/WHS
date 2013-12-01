8f673c979a12f17136cc5ee80b6e3225447dc298
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-11-30 21:35:53 -0800

    slugs

--- humanesociety/apps/news/models.py
+++ humanesociety/apps/news/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+
 class Entry(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
     image = models.ImageField(null=True, blank=True, upload_to='entries')
     date_created = models.DateTimeField(auto_now_add=True)
+    tease = models.TextField(null=True, blank=True)
+    body = models.TextField()
 
     class Meta:
         verbose_name_plural = 'entires'
+        ordering = ['-date_created', ]
