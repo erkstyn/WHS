3ed762a255cedfad467f192ecb1461dc4cc666f7
Chris Dickinson <christopher.s.dickinson@gmail.com>
2014-05-19 11:30:13 -0700

    just use view functions

--- humanesociety/www/views.py
+++ humanesociety/www/views.py
@@ -1,21 +1,10 @@
-from django.views.generic.base import TemplateView
+from django.shortcuts import render
+
 from apps.news.models import Entry
 
-
-class HomePageView(TemplateView):
-    template_name = 'homepage.html'
-
-    def get_context_data(self, request, **kwargs):
-        context = super(HomePageView, self).get_context_data(**kwargs)
-        context.update({
-            # add key-value pairs here
-            'posts': (
-                Entry.objects.all()
-                if request.user.is_superuser else
-                Entry.objects.filter(status=Entry.PUBLISHED)
-            )[:10]
-        })
-        return context
-
-
-homepage = HomePageView.as_view()
+def homepage(request):
+    return render(request, 'homepage.html', {
+        'posts': Entry.objects.all()
+        if request.user.is_superuser else
+        Entry.objects.filter(status=Entry.PUBLISHED)
+    })
